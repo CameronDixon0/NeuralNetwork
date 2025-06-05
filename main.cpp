f5839e08255129ad7e141b3faf3cfe7ec19d3682
CameronDixon0 <109122993+CameronDixon0@users.noreply.github.com>
2025-06-05 14:33:29 +1000

    learning code

--- main.cpp
+++ main.cpp
@@ -6,36 +6,36 @@
 int main () {
   std::system("clear");
 
-  // std::ifstream trainingImages ("../mnist/train-images.idx3-ubyte");
-  // std::vector<std::vector<float>> numbers;
-  // mnist::readAllToVector(trainingImages, numbers);
-  // trainingImages.close();
+  std::ifstream trainingImages ("../mnist/train-images.idx3-ubyte");
+  std::vector<std::vector<float>> numbers;
+  mnist::readAllToVector(trainingImages, numbers);
+  trainingImages.close();
 
-  // std::ifstream trainingLabels ("../mnist/train-labels.idx1-ubyte");
-  // std::vector<int> labels;
-  // mnist::readLabelsToVector(trainingLabels, labels);
-  // trainingLabels.close(); 
+  std::ifstream trainingLabels ("../mnist/train-labels.idx1-ubyte");
+  std::vector<int> labels;
+  mnist::readLabelsToVector(trainingLabels, labels);
+  trainingLabels.close(); 
 
-  // nnet::Network net({784, 256, 128, 64, 10});
-  // for (int _ = 0; _ < 100; _++) {
-  //   std::cout << "Iteration " << _ + 1 << std::endl;
-  //   for (int i = 0; i < 60000; i++) {
-  //     std::vector<float>& num = numbers[i];
-  //     nnet::Vector vec(num);
+  nnet::Network net({784, 256, 128, 64, 10});
+  for (int _ = 0; _ < 100; _++) {
+   std::cout << "Iteration " << _ + 1 << std::endl;
+   for (int i = 0; i < 60000; i++) {
+     std::vector<float>& num = numbers[i];
+     nnet::Vector vec(num);
 
-  //     int label = labels[i];
+     int label = labels[i];
 
-  //     net.feedForward(vec);
-      
-  //     nnet::Vector expected(10, 0);
-  //     expected[label] = 1;
-  //     net.backProp(expected, 0.005 / (_ + 1));
-  //   }
-  // }
+     net.feedForward(vec);
 
-  // std::ofstream outFile ("/Users/camerondixon/Development/Projects/C++/NeuralNet/trainedNetwork", std::ios::binary);
-  // net.saveToFile(outFile);
-  // outFile.close();
+     nnet::Vector expected(10, 0);
+     expected[label] = 1;
+     net.backProp(expected, 0.005 / (_ + 1));
+   }
+  }
+
+   std::ofstream outFile ("/Users/camerondixon/Development/Projects/C++/NeuralNet/trainedNetwork", std::ios::binary);
+   net.saveToFile(outFile);
+   outFile.close();
 
   std::ifstream testNumbersFile ("../mnist/t10k-images.idx3-ubyte");
   std::vector<std::vector<float>> testNumbers;
